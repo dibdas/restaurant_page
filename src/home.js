ca5bb0c9a14535e4a708d356f26bb290372ec2eb
dibdas <dibyandu08das@gmail.com>
2021-04-18 01:41:53 +0530

    modified home.js

--- src/home.js
+++ src/home.js
@@ -9,9 +9,9 @@ element.innerHTML =`
 <div class="container-fluid menu">
 <ul class="menu-list">
 <li class="menu-item"><a href="#home"class="nav-menu-link">Home</a></li>
-<li class="menu-item"><a href="#menu" class="nav-menu-link">Menu</a></li>
+<li class="menu-item"><a href="#menu" class="nav-menu-link" id="menu">Menu</a></li>
 <li class="menu-item"><a href="#about" class="nav-menu-link" id="about">About Us</a></li>
-<li class="menu-item"><a href="#contact" id="contact" >Contact</a></li>
+<li class="menu-item"><a href="#contact" class="nav-menu-link" id="contact" >Contact</a></li>
 </ul>
 </div>
 </header>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import home from './home';
 import contactPage from './contactPage';
 import about from './about';
+import menu from './menu';
+
 home();
 
 const about_us = document.querySelector('#about');
-about_us.addEventListener('click',()=>about())
+about_us.addEventListener('click',()=> about())
+
+const menu_list = document.querySelector('#menu')
+menu_list.addEventListener('click',()=> menu())
 
 const contact = document.querySelector('#contact');
 contact.addEventListener('click', () => contactPage())
