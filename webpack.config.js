19876118fe075768bc7a8126c41707797b7d084d
dibdas <dibyandu08das@gmail.com>
2021-04-25 01:34:05 +0530

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   devServer: {
-      contentBase: path.join(__dirname,'dist'),
-      compress: true
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
   },
 };
