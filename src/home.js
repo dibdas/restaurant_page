0e32684abcdc852b510db8f8c2b9d7e137cb3df9
dibdas <dibyandu08das@gmail.com>
2021-04-25 15:21:25 +0530

    removed li of home

--- src/home.js
+++ src/home.js
@@ -12,7 +12,6 @@ const home = () => {
   </div>
   <div class="menu">
   <ul class="menu-list">
-  <li class="menu-item"><a href="#home"class="nav-menu-link">Home</a></li>
   <li class="menu-item"><a href="#menu" class="nav-menu-link" id="menu">Menu</a></li>
   <li class="menu-item"><a href="#about" class="nav-menu-link" id="about">About Us</a></li>
   <li class="menu-item"><a href="#contact" class="nav-menu-link" id="contact" >Contact</a></li>
